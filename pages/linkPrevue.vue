1bd3321197561eb420f439d6a5cb452b3e2630cc
Spanaic <spanaic@gmail.com>
2020-01-26 17:10:41 +0900

    [change] 複数の値をbindしてurlをpreview表示する

--- pages/linkPrevue.vue
+++ pages/linkPrevue.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <div>
-      <a :href="link">
-        <link-prevue :url="link" :onButtonClick="onClick"></link-prevue>
-      </a>
-    </div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" lg="4" md="8" sm="12" v-for="(item, i) in items.link" :key="i">
+          <a :href="item">
+            <link-prevue :style="width" :url="item" :onButtonClick="onClick(item)"></link-prevue>
+          </a>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import LinkPrevue from "link-prevue";
+// import LinkPrevue from "link-prevue";
+import LinkPrevue from "@/components/LinkPrevue";
 
 export default {
   components: {
@@ -18,22 +23,31 @@ export default {
   data() {
     return {
       //   link: "https://vuejs.org/"
-      link: "https://mj-nanikiru.net/"
+      //   link: "https://mj-nanikiru.net/",
+      items: {
+        link: [
+          "https://www.google.com/",
+          "https://www.amazon.co.jp/",
+          "https://www.apple.com/"
+        ]
+      },
+      item: "",
+      width: "300px"
     };
   },
   methods: {
     onClick(prevue) {
       console.log(
-        "click",
-        prevue.images,
-        prevue.title,
-        prevue.url,
-        prevue.description
+        "click"
+        // prevue.images,
+        // prevue.title,
+        // prevue.url,
+        // prevue.description
       );
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
 </style>
